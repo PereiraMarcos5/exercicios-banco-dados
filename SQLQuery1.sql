46644ed06d80263894d0847a32f81580eea1bac9
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-06-21 17:20:48 -0300

    Iniciado e Finalizado primeira SQLs

---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Endereço3 (
+nome VARCHAR(100),
+cpf VARCHAR(14),
+rg VARCHAR(12),
+datadeNascimento Date,
+idade TINYINT
+);
+TRUNCATE TABLE Endereço3;
+INSERT INTO Endereço3 VALUES('Erick Bryan Enrico Vieira', 699734958-70, 90745276-0, '1989-05-28', 29);
+INSERT INTO Endereço3 VALUES('Samuel Ruan Dias', 802790194-40, 553186917, '1988-02-08', 30);
+INSERT INTO Endereço3 VALUES('Fábio Benjamin Souza', 522445349-60, 547152322, '1970-03-22', 48);
+INSERT INTO Endereço3 VALUES('Elias Miguel Aparicio', 293928821-65, 126804448, '1986-06-07', 32);
+INSERT INTO Endereço3 VALUES('Alexandre Gustavo Cardoso', 617408878-24, 88885744, '2003-04-15', 15);
+INSERT INTO Endereço3 VALUES('Kauê Lucca Duarte', 943163990-47, 469775099, '1965-06-15', 53);
+INSERT INTO Endereço3 VALUES('Marcos Bryan Guilherme Mendes', 955129235-95, 84050077-4, '2015-02-06',3);
+INSERT INTO Endereço3 VALUES('Benedito Pedro Carlos da Mota', 510505472-50, 43588124-3, '1981-04-19', 37);
+INSERT INTO Endereço3 VALUES('Luís Igor Barbosa', 370570311-06, 40651407-0, '2013-05-06', 5);
+INSERT INTO Endereço3 VALUES('José Heitor Ramos', 675624925-81, 83962258-2, '1965-04-11', 53);
+INSERT INTO Endereço3 VALUES('Lucca Tiago Galvão', 788124508-57, 3293335-6, '1977-06-17', 41);
+INSERT INTO Endereço3 VALUES('Tiago Edson Oliveira', 389969352-39, 8198446-9, '1977-06-09', 41);
+INSERT INTO Endereço3 VALUES('Enrico Julio Fábio Martins', 529078166-83, 99886242-3, '2017-03-11', 1);
+INSERT INTO Endereço3 VALUES('Oliver Victor Benjamin da Mota', 798556805-02, 5103578-9, '1985-03-27', 33);
+INSERT INTO Endereço3 VALUES('Bryan Caio Lopes', 213217201-30, 12949454-9, '1970-06-18', 48);
