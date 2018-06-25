a3d6229ea29c7b2219a6db3a333632c86dc78d80
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-06-25 16:39:19 -0300

    Listas

---
+++ SQLExercicioLista2.sql
@@ -0,0 +1,75 @@
+﻿-- SELECT * FROM pokemons;
+
+/*
+SELECT 
+nome, ataque, especial_ataque,
+especial_defesa, defesa
+FROM pokemons
+ORDER BY nome, ataque;
+*/
+
+/*
+SELECT 
+nome, categoria, ataque
+FROM pokemons
+ORDER BY nome, ataque;
+*/
+
+/*
+SELECT
+nome, altura, peso, 
+(peso / altura * altura) 'Imc'
+FROM pokemons
+*/
+
+/*
+SELECT
+nome, altura, peso, 
+(peso / altura * altura) 'Imc'
+FROM pokemons
+ORDER BY Imc DESC;
+*/
+
+/*
+SELECT
+nome, LEN(nome) 'Length'
+FROM pokemons
+ORDER BY 'Length' DESC;
+*/
+
+/*
+SELECT
+nome, descricao, LEN(nome) 'Caracteres do Nome'
+FROM pokemons
+WHERE LEN(nome) > 10 
+ORDER BY nome;
+*/
+
+/*
+SELECT 
+nome, categoria, ataque
+FROM pokemons
+WHERE ataque = (SELECT MIN(ataque) FROM pokemons);
+*/
+
+/*
+SELECT ataque, especial_ataque, nome, defesa, especial_defesa
+FROM pokemons
+ORDER BY ataque, especial_ataque, nome, defesa, especial_defesa;
+*/
+
+/*
+SELECT AVG(ataque) FROM pokemons;
+*/
+
+/*
+SELECT SUM(ataque) FROM pokemons;
+*/
+
+/*
+SELECT AVG(especial_ataque)
+FROM pokemons
+WHERE nome LIKE '%P%';
+*/
+
+
---
+++ SQLListaExercicioLista3.sql
@@ -0,0 +1,26 @@
+﻿/*
+SELECT * FROM alunos
+*/
+
+/*
+SELECT nome, nota_1
+FROM alunos
+WHERE nota_1 > 9.0;
+*/
+
+/*
+SELECT nome, nota_1, nota_2, nota_3, nota_4,
+(nota_1 + nota_2 + nota_3 + nota_4 /4) 'Média'
+FROM alunos
+*/
+
+/*
+SELECT  Count(signo) 
+FROM alunos 
+WHERE signo LIKE 'peixes'
+*/
+
+/*
+SELECT SUM(nota_1) 'Soma nota 1'
+FROM alunos
+*/
