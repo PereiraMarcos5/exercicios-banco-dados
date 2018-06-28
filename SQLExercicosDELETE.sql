fa9b9ed0b4afaf98b9963bef9203a68c8f6222bf
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-06-28 17:28:53 -0300

    Feito

--- SQLExercicosDELETE.sql
+++ SQLExercicosDELETE.sql
@@ -1,4 +1,4 @@
-﻿-- SELECT * FROM pokemons
+﻿SELECT * FROM cidades
 
 /*
 DELETE FROM pokemons WHERE categoria LIKE 'Seed'
@@ -49,5 +49,42 @@ DELETE FROM pokemons WHERE descricao LIKE 'Shell'
 DELETE FROM pokemons WHERE peso >= 100
 */
 
-
+/*
+DELETE FROM pokemons WHERE altura <= 1
+*/
  
+/*
+DELETE FROM pokemons WHERE especial_defesa > 3
+*/
+
+/*
+DELETE FROM pokemons WHERE LEN(descricao) > 200
+*/
+
+/*
+DELETE FROM pokemons WHERE descricao LIKE '%good%'
+*/
+
+/*
+DELETE FROM pokemons WHERE codigo % 2 = 0 
+*/
+
+/*
+DELETE FROM pokemons WHERE nome LIKE 'Lileep'
+*/
+
+/*
+DELETE FROM pokemons WHERE especial_ataque = 5
+*/
+
+/*
+DELETE FROM pokemons WHERE codigo < 100
+*/
+
+/*
+DELETE from cidades WHERE estado = 'RS'
+*/
+
+/*
+DELETE from cidades WHERE estado = 'AC' AND cidade LIKE 'R%'
+*/
